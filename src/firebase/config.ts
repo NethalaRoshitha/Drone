5305c867d831ed584248242d1c8796cb5ee8ba29
Roshitha Nethala <nethalaroshitha02@gmail.com>
2026-02-25 05:49:36 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-4533418326-c7509",
+  "appId": "1:732006948696:web:45be9bbdb023ee9a978f2d",
+  "apiKey": "AIzaSyDRnl-G_e77qG0daxb7WupPsS2UYBf_fYE",
+  "authDomain": "studio-4533418326-c7509.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "732006948696"
+};
